f4ee3f1bb3adc6fcc0be6c00663335c5f2e8edcd
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-06-19 17:26:47 -0300

    feat: add CRUD Category

---
+++ app/Services/CategoryServices.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+use App\Models\Category;
+
+class CategoryServices 
+{
+
+  public function list()
+  {
+    $categories = Category::paginate(); // paginate(15) is default
+    
+    return $categories;
+  }
+
+  public function store(CategoryStoreRequest $request)
+  {
+    $category = Category::create($request->validated());
+    
+    return $category;
+  }
+
+  public function update(CategoryUpdateRequest $request, Category $category)
+  {
+    $category->update($request->validated());
+    
+    return $category;
+  }
+
+  public function destroy(Category $category)
+  {
+    $category->delete();
+  }
+}
