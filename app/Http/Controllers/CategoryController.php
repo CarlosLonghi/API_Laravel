9b482d797b1b5e5eabad15eff3415e905f0b5be0
CarlosLonghi <carlos.longhi@digitalone.com.br>
2024-06-19 17:33:46 -0300

    fix: import Gate in CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\CategoryUpdateRequest;
 use App\Models\Category;
 use App\Services\CategoryServices;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class CategoryController extends Controller
 {
